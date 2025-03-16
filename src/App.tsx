cee9f277a1441bfacdf2bc4348425011c1bfabf2
Mohd Ziyad <mziyadc@gmail.com>
2025-03-16 22:53:46 +0530

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <div className="flex justify-center items-center text-7xl">
+        Hello Zokooo
+      </div>
+    </>
+  );
+}
+
+export default App;
