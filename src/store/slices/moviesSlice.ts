3a257979ca745dd4f319360cb1b6bc053b365ca9
Mohd Ziyad <mziyadc@gmail.com>
2025-03-18 23:52:02 +0530

    Added the movie slice

--- src/store/slices/moviesSlice.ts
+++ src/store/slices/moviesSlice.ts
@@ -1,5 +1,6 @@
+import { getMovieDetails, searchMovies } from "@/api/api";
 import { Movie, MovieDetails } from "@/types";
-import { createSlice, PayloadAction } from "@reduxjs/toolkit";
+import { createAsyncThunk, createSlice, PayloadAction } from "@reduxjs/toolkit";
 
 interface MovieState {
   searchResults: Movie[];
@@ -39,4 +40,62 @@ const moviesSlice = createSlice({
       state.status = "idle";
     },
   },
+  extraReducers: (builder) => {
+    builder
+      .addCase(fetchMoviesBySearch.pending, (state) => {
+        state.status = "loading";
+      })
+      .addCase(fetchMoviesBySearch.fulfilled, (state, action) => {
+        state.status = "succeeded";
+
+        if (action.meta.arg.page === 1) {
+          state.searchResults = action.payload.Search || [];
+        } else {
+          state.searchResults = [
+            ...state.searchResults,
+            ...(action.payload.Search || []),
+          ];
+        }
+
+        state.totalResults = parseInt(action.payload.totalResults || "0", 10);
+        state.currentPage = action.meta.arg.page || 1;
+        state.error = null;
+      })
+      .addCase(fetchMoviesBySearch.rejected, (state, action) => {
+        state.status = "failed";
+        state.error = action.error.message || "Failed to fetch movies";
+      })
+      .addCase(fetchMovieDetails.pending, (state) => {
+        state.status = "loading";
+      })
+      .addCase(fetchMovieDetails.fulfilled, (state, action) => {
+        state.status = "succeeded";
+        state.selectedMovie = action.payload;
+        state.error = null;
+      })
+      .addCase(fetchMovieDetails.rejected, (state, action) => {
+        state.status = "failed";
+        state.error = action.error.message || "Failed to fetch movie details";
+      });
+  },
 });
+
+export const fetchMoviesBySearch = createAsyncThunk(
+  "movies/fetchMoviesBySearch",
+  async (params: { searchTerm: string; page?: number }) => {
+    const response = await searchMovies(params.searchTerm, params.page);
+    return response;
+  }
+);
+
+export const fetchMovieDetails = createAsyncThunk(
+  "movies/fetchMovieDetails",
+  async (id: string) => {
+    const response = await getMovieDetails(id);
+    return response;
+  }
+);
+
+export const { setSearchTerm, resetSearch, clearSelectedMovie } =
+  moviesSlice.actions;
+export default moviesSlice;
