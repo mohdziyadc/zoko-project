75a250eb8c59ce605ebf36af344224c77d814c0c
Mohd Ziyad <mziyadc@gmail.com>
2025-03-20 11:08:49 +0530

    persisted search state across navigation

--- src/components/SearchBar.tsx
+++ src/components/SearchBar.tsx
@@ -1,21 +1,49 @@
-import { useEffect } from "react";
+import { useAppDispatch, useAppSelector } from "@/hooks/reduxHooks";
 import { Input } from "./ui/input";
-import { useMovieSearch } from "@/hooks/useMovieSearch";
+import { fetchMoviesBySearch, setSearchTerm } from "@/store/slices/moviesSlice";
+import { useEffect, useRef, useState } from "react";
 
 const SearchBar = () => {
-  const { searchTerm, setSearchTerm } = useMovieSearch();
+  const dispatch = useAppDispatch();
+  const { searchTerm } = useAppSelector((state) => state.movies);
+  const [localSearchTerm, setLocalSearchTerm] = useState("");
+
+  const timeoutRef = useRef<NodeJS.Timeout | null>(null);
+
+  const handleSearch = (value: string, delay: number = 500) => {
+    setLocalSearchTerm(value);
+    if (timeoutRef.current) {
+      clearTimeout(timeoutRef.current);
+    }
+
+    timeoutRef.current = setTimeout(() => {
+      dispatch(setSearchTerm(value));
+      if (value.trim()) {
+        dispatch(fetchMoviesBySearch({ searchTerm: value.trim(), page: 1 }));
+      }
+    }, delay);
+  };
 
   useEffect(() => {
-    console.log("Redux searchTerm:", searchTerm);
-  });
+    setLocalSearchTerm(searchTerm);
+  }, [searchTerm]);
+
+  useEffect(() => {
+    return () => {
+      if (timeoutRef.current) {
+        clearTimeout(timeoutRef.current);
+      }
+    };
+  }, []);
+
   return (
     <div>
       <Input
         type={"text"}
         placeholder="Search for movies"
-        value={searchTerm}
+        value={localSearchTerm}
         className="rounded-lg p-4"
-        onChange={(e) => setSearchTerm(e.target.value)}
+        onChange={(e) => handleSearch(e.target.value)}
       />
     </div>
   );
